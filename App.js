8e72742d38a64141771835cba5e6312c38beae24
Django L. Kerger <djangol.kerger@MacBook-Pro.attlocal.net>
2020-06-26 10:17:39 -0500

    search by city for weather

--- App.js
+++ App.js
@@ -1,26 +1,40 @@
 const express = require("express");
-const https = require("https")
+const https = require("https");
+const bodyParser = require("body-parser")
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", function(req,res){
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=houston&units=imperial&appid=3df84af1828eb0aaca341401c109840c"
+    res.sendFile(__dirname + "/index.html")
+      
+});
+
+app.post("/", function (req,res){
+
+    const query = req.body.cityName;
+    const apiKey = "3df84af1828eb0aaca341401c109840c";
+    const unit = "imperial";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="+ query +"&units="+ unit +"&appid="+ apiKey
     https.get(url, function(response){
 
         response.on("data", function(data){
-            const weatherData = JSON.parse(data)
+            const weatherData = JSON.parse(data);
             const temp = weatherData.main.temp
-            const weatherDescription = weatherData.weather[0].description
-            const icon = weatherData.weather[0].icon
-            const imgURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
-            res.write("<p>the weather is currently " + weatherDescription + "</p>")
-            res.write("<h1>The temperature in Houston is "+ temp + " degrees Fahrenheit</h1>")
+            const weatherDescription = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon;
+            const imgURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+            res.write("<p>the weather is currently " + weatherDescription + "</p>");
+            res.write("<h1>The temperature in " + query + " is "+ temp + " degrees Fahrenheit</h1>")
             res.write("<img src=" + imgURL + ">")
             res.send()
             
         })
-    })  
+    })
 })
+
+
 
 
 app.listen(3000, function() {
